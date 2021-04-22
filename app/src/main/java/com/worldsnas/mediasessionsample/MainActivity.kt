961d681d1f81dd3ecc162d9cdebca98bb637893d
worldsnas <worldsnas@gmail.com>
2021-04-22 02:37:34 +0200

    download single surah and unzip in the package

--- app/src/main/java/com/worldsnas/mediasessionsample/MainActivity.kt
+++ app/src/main/java/com/worldsnas/mediasessionsample/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.worldsnas.mediasessionsample
 
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.worldsnas.mediasessionsample.AyaPlayList.Companion.EXTRA_KEY_AYA_MEDIA_ITEM
+import java.io.*
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,20 +20,19 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
+            unzip()
+//            startService(Intent(this, PlayerService::class.java).apply {
+//                action = PlayerAction.Play.action
+//                putExtra(EXTRA_KEY_AYA_MEDIA_ITEM, AyaPlayList(
+//                    AyaPlayList.StartingAya.Beginning,
+//                    1,
+//                    AyaPlayList.Reciter(1),
+//                    AyaPlayList.Part.Surah(1)
+//                ))
+//            })
 
-            startService(Intent(this, PlayerService::class.java).apply {
-                action = PlayerAction.Play.action
-                putExtra(EXTRA_KEY_AYA_MEDIA_ITEM, AyaPlayList(
-                    AyaPlayList.StartingAya.Beginning,
-                    1,
-                    AyaPlayList.Reciter(1),
-                    AyaPlayList.Part.Surah(1)
-                ))
-            })
-
-            startService(Intent(this, PlayerService::class.java))
+//            startService(Intent(this, PlayerService::class.java))
         }
-
         Build.VERSION_CODES.Q
     }
 
@@ -51,6 +52,30 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+
+    fun unzip() {
+//        val gzipSource = assets.open("surah.zip").source().gzip()
+//        gzipSource.buffer().buffer.use { buffer ->
+//            File(getExternalFilesDir(null)!!, "someFile").apply {
+//                if (exists()) {
+//                    delete()
+//                }
+//
+//                createNewFile()
+//            }.sink().use {
+//                it.write(buffer, buffer.size)
+//            }
+//        }
+
+
+//        unzip(assets.open("surah.zip"), getExternalFilesDir(null)!!)
+
+        Log.d("test", "finished")
+
+    }
+
+
 }
 
 
--- app/src/main/java/com/worldsnas/mediasessionsample/PlaylistManager.kt
+++ app/src/main/java/com/worldsnas/mediasessionsample/PlaylistManager.kt
@@ -3,19 +3,24 @@ package com.worldsnas.mediasessionsample
 import android.content.SharedPreferences
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import java.io.File
+import okhttp3.internal.closeQuietly
+import okio.*
+import java.io.*
+import java.io.IOException
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
 
 class PlaylistManager(
-    private val downloadDir : File,
-    private val playerView : PlayerView,
-    private val client : OkHttpClient,
+    private val downloadDir: File,
+    private val playerView: PlayerView,
+    private val client: OkHttpClient,
     private val sharedPreferences: SharedPreferences,
 ) {
 
-    fun getPlayListItems(playList : AyaPlayList){
+    fun getPlayListItems(playList: AyaPlayList) {
         val reciterDir = File(downloadDir, playList.reciter.id.toString())
 
-        if(!reciterDir.exists()){
+        if (!reciterDir.exists()) {
 
             //TODO start downloading the recites
             return
@@ -25,14 +30,14 @@ class PlaylistManager(
             is AyaPlayList.Part.Aya -> {
                 val surahDir = File(reciterDir, playList.part.surahOrder.toString())
 
-                if (!surahDir.exists()){
+                if (!surahDir.exists()) {
                     // TODO download
                     return
                 }
 
                 val audioFile = File(surahDir, playList.order.orderId.toString() + ".mp3")
 
-                if(!audioFile.exists()){
+                if (!audioFile.exists()) {
                     // TODO download
 
                     return
@@ -43,7 +48,7 @@ class PlaylistManager(
             is AyaPlayList.Part.Surah -> {
                 val surahDir = File(reciterDir, playList.part.order.toString())
 
-                if (!surahDir.exists()){
+                if (!surahDir.exists()) {
                     // TODO download
                     return
                 }
@@ -57,12 +62,113 @@ class PlaylistManager(
         }
     }
 
-    private fun downloadRecite(playList : AyaPlayList){
+    private fun downloadAndUnZipSurah(reciterId : String, surahOrder : String) {
+        val reciteDirectory = File(downloadDir, reciterId)
 
-//        Request.Builder()
-//            .
-//        client.newCall()
+        if (!reciteDirectory.exists()) {
+            reciteDirectory.mkdirs()
+        }
 
+        val downloadedSurahZip = File(reciteDirectory, "$surahOrder.zip")
+        if(!downloadedSurahZip.exists())
+            downloadSurahZip(reciterId, surahOrder)
+
+        // we should unzip it now
+        unzip(downloadedSurahZip, File(reciteDirectory, surahOrder))
+
+        //we delete to avoid the extra space
+        downloadedSurahZip.delete()
+
+        //folder has been downloaded
+    }
+
+    private fun downloadSurahZip(reciterId : String, surahOrder : String) {
+        val reciteDirectory = File(downloadDir, reciterId)
+
+        val tempReciteFile = File(reciteDirectory, "$surahOrder$PREFIX_ZIP_FILE_TEMP.zip")
+        if (tempReciteFile.exists()) {
+            //TODO support resuming
+            tempReciteFile.delete()
+            tempReciteFile.createNewFile()
+        }
+
+
+        var sink : Sink? = null
+        var source : BufferedSource? = null
+        try {
+            val request = Request.Builder().url("").build()
+
+            val response = client.newCall(request).execute()
+            val body = response.body ?: TODO("handle errors")
+            val contentLength = body.contentLength()
+
+            source = body.source()
+            sink = tempReciteFile.sink().buffer().buffer
+
+            var totalRead = 0L
+            var bufferSize: Long = 8 * 1024
+            var bytesRead = 0L;
+
+            bytesRead = source.read(sink, bufferSize)
+            while (bytesRead != -1L) {
+                sink.emit();
+                totalRead += bytesRead;
+                val progress = ((totalRead * 100) / contentLength).toInt()
+
+                //TODO publish progress
+//            subscriber.onNext(progress);
+
+
+                bytesRead = source.read(sink, bufferSize)
+            }
+            sink.flush();
+        } catch (e : IOException) {
+            e.printStackTrace();
+            //TODO handle download errors
+        } finally {
+            source?.closeQuietly()
+            sink?.closeQuietly()
+        }
+
+        //rename the file so we know it's been downloaded completely
+        tempReciteFile.renameTo(File(tempReciteFile.absolutePath.replace(PREFIX_ZIP_FILE_TEMP, "")))
 
     }
+
+    @Throws(IOException::class)
+    fun unzip(zipFile: File, targetDirectory: File) {
+        val zis = ZipInputStream(
+            BufferedInputStream(FileInputStream(zipFile))
+        )
+        try {
+            var ze: ZipEntry? = zis.nextEntry
+            var count: Int
+            val buffer = ByteArray(8192)
+            while (ze != null) {
+                val file = File(targetDirectory, ze.name)
+                val dir = if (ze.isDirectory) file else file.parentFile
+                if (!dir.isDirectory && !dir.mkdirs()) throw FileNotFoundException(
+                    "Failed to ensure directory: " +
+                            dir.absolutePath
+                )
+                if (ze.isDirectory) continue
+                val fout = FileOutputStream(file)
+                try {
+                    while (zis.read(buffer).also { count = it } != -1) fout.write(buffer, 0, count)
+                } finally {
+                    fout.close()
+                }
+                /* if time should be restored as well
+            long time = ze.getTime();
+            if (time > 0)
+                file.setLastModified(time);
+            */
+                ze = zis.nextEntry
+            }
+        } finally {
+            zis.close()
+        }
+    }
 }
+
+const val PREFIX_ZIP_FILE_TEMP = "-temp"
